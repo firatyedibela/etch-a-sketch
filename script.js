03288f2b58fcd6c9a1f4e09f60ac06059fbc9f33
Firat Yedibela <yedibelafirat0@gmail.com>
2023-12-15 11:43:59 +0300

    Create reset grid logic
    Implement resetGrid() function and call it whenever the reset button is clicked

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const gridContainer = document.querySelector('#grid-container');
+const resetGridBtn = document.querySelector('#reset-grid');
 
-let squaresPerSide = 20;
-gridBuilder(squaresPerSide);
+let squaresPerSide = 16;
+buildGrid(squaresPerSide);
 
-// Get all the cells in the page so we can add event listeners to them
-let cells = document.querySelectorAll('.columns');
-cells.forEach((cell) => {
-  cell.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = 'black';
-  });
-});
+resetGridBtn.onclick = resetGrid;
 
-
-function gridBuilder(squaresPerSide) {
+function buildGrid(squaresPerSide) {
    for (let i = 0; i < squaresPerSide; i++) {
     const rows = document.createElement('div');
     rows.classList.add('rows');
@@ -24,4 +18,19 @@ function gridBuilder(squaresPerSide) {
       rows.appendChild(columns);
     };
    };
+   // Register event listeners to all squares to paint them black   when hovered
+    let cells = document.querySelectorAll('.columns');
+    cells.forEach((cell) => {
+      cell.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'black';
+      });
+    });
+}
+
+function resetGrid() {
+  // Delete all squares and create again
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  buildGrid(squaresPerSide);
 }
