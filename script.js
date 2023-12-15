4a3e8da835ff1ae15acdc5a0dcdb2cfd1ee872d9
Firat Yedibela <yedibelafirat0@gmail.com>
2023-12-15 10:01:52 +0300

    Handle painting logic
    Add event listeners to all cells using DOM methods and make them black if mouse moves over them

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const gridContainer = document.querySelector('#grid-container');
 
 let squaresPerSide = 20;
-
 gridBuilder(squaresPerSide);
+
+// Get all the cells in the page so we can add event listeners to them
+let cells = document.querySelectorAll('.columns');
+cells.forEach((cell) => {
+  cell.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'black';
+  });
+});
+
 
 function gridBuilder(squaresPerSide) {
    for (let i = 0; i < squaresPerSide; i++) {
